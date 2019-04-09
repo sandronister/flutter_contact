00598cd3af932300f200140c2c1f79c62dd21c5b
julio <sandronister@gmail.com>
2019-04-09 20:36:59 -0300

    fitst commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_contact/ui/ContactPage.dart';
+
+
+void main(){
+  runApp(MaterialApp(
+    home:ContactPage()
+  )
+  );
+}
---
+++ lib/ui/ContactPage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ContactPage extends StatefulWidget {
+  @override
+  _ContactPageState createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text('Contato'),
+        ),
+        backgroundColor: Colors.red,
+      ),
+    );clclc
+  }
+}
+    
